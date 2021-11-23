bcc3f088ead1023faee0b4fa89e709e0e1310436
Stian Knudsen <stian.knudsen@bir.no>
2021-11-23 07:44:46 +0100

    working test

--- moxa_telnet.py
+++ moxa_telnet.py
@@ -4,7 +4,7 @@
 import tftpy
 import telnetlib
 import subprocess
-from time import sleep
+from time import sleep, time
 from multiprocessing import Process
 
 
@@ -16,28 +16,24 @@ ethernet_card = "enp0s31f6"
 switch_pre_ip = "192.168.127.253"
 laptop_pre_ip ='192.168.127.200'
 
-def change_ip(ip):
+def get_ip():
     get_ip = subprocess.run(["ip addr show " + ethernet_card], shell=True, capture_output=True, encoding='ascii')
     slicepos = get_ip.stdout.find('inet ')
     current_ip = ''.join(get_ip.stdout[slicepos+5:slicepos+20])
+    return current_ip
+
+def change_ip(ip, current_ip):
     if ip != current_ip:
+        # remove from last dot, not last 3 spaces in case of ip address under 100:
         set_ip = subprocess.run(["sudo ip addr add local " + ip + "/24 broadcast " + ip[:-3] + "255 dev " + ethernet_card], shell=True)
         if set_ip.returncode == 0:
-            print("IP addresse: {}".format(ip))
+            print("IP address set to: {}".format(ip))
         else:
-            print("Kunne ikke sette ip")
+            print("Could not set IP...")
             exit(-1)
     else:
-        print("Har allerede IP: ", current_ip)
+        print("Already have IP: ", current_ip)
 
-"""
-
-terminal length 0
-show relay warning
-
-
-
-"""
 
 def login(ip):
     tn = telnetlib.Telnet(ip)
@@ -76,15 +72,27 @@ def login(ip):
         tn.write(password.encode('ascii') + b'\n')  # Enter password
         tn.write('\n'.encode('ascii'))
         tn.read_until(b'EDS-408A-MM-SC#')
+        # ----- info:
+        tn.write(b'terminal length 0\n')
+        tn.read_until(b'EDS-408A-MM-SC#')
+        tn.write(b'show system\n')
+        sysinfo = tn.read_until(b'EDS-408A-MM-SC#').decode('ascii')
+        tn.write(b'show version\n')
+        version = tn.read_until(b'EDS-408A-MM-SC#').decode('ascii')
+        print(sysinfo)
+        print(version)
+        # -----
         server = tftpy.TftpServer('')
         # ----------------------------------------------------------------------------- logged in
         while True:
             print('-' * 25)
             print('1. Update Firmware')
             print('2. Download Running Config')
-            print('3. Download Startip Config')
+            print('3. Download Startup Config')
             print('4. Upload Config')
             print('5. Set login mode to menu')
+            print('6. Show relay-warnings')
+            print('8. Change Name of switch')
             print('-' * 25)
             choice = input('What do you want to do: ')
             if choice == '':
@@ -125,16 +133,50 @@ def login(ip):
             if int(choice) == 5:
                 tn.write(b'login mode menu\n')
                 break
+            if int(choice) == 6:
+                tn.write(b'terminal length 0\n')
+                tn.read_until(b'EDS-408A-MM-SC#')
+                tn.write(b'show relay-warning config\n')
+                relay_text = tn.read_until(b'EDS-408A-MM-SC#')
+                print(relay_text.decode('ascii'))
+            if int(choice) == 7:
+                tn.write(b'terminal length 0\n')
+                tn.read_until(b'EDS-408A-MM-SC#')
+                tn.write(b'configure\n')
+                tn.read_until(b'EDS-408A-MM-SC(config)#')
+                tn.write(b'interface mgmt\n')
+                tn.read_until(b'EDS-408A-MM-SC(config-vlan)#')
+                new_ip = input('Please input last digits of ip: ').encode('ascii')
+                tn.write(b'ip address static 192.168.127.' + new_ip + b' 255.255.255.0\n')
+                tn.write(b'exit\n')
+                relay_text = tn.read_until(b'EDS-408A-MM-SC#')
+                # print(relay_text)
+            if int(choice) == 8:
+                tn.write(b'terminal length 0\n')
+                tn.read_until(b'EDS-408A-MM-SC#')
+                tn.write(b'configure\n')
+                relay_text = tn.read_until(b'EDS-408A-MM-SC(config)#')
+                hostname = input('Switch Name: ')
+                tn.write(b'hostname ' + hostname.encode('ascii') + b'\n')
+                relay_text = tn.read_until(b'EDS-408A-MM-SC(config)#')
+                tn.write(b'exit\n')
+                tn.read_until(b'EDS-408A-MM-SC#')
+            
 
 if __name__ == "__main__":
     while True:
-        print('1. Telnet options')
-        print('2. Change laptop ip')
-        choice = input('>_  ')
-        if str(choice) == '':
-            break
-        if int(choice) == 1:
+        current_ip = get_ip()
+        if current_ip == laptop_pre_ip:
             login(switch_pre_ip)
             break
-        if int(choice) == 2:
-            change_ip(laptop_pre_ip)
+        else:
+            print('1. Telnet options')
+            print('2. Change laptop ip')
+            choice = input('>_  ')
+            if str(choice) == '':
+                break
+            if int(choice) == 1:
+                login(switch_pre_ip)
+                break
+            if int(choice) == 2:
+                change_ip(laptop_pre_ip, current_ip)

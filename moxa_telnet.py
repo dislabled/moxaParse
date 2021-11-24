2b1df58d92e78b1e7983ff4458542e5a3ad543cb
Stian Knudsen <dislabled@gmail.com>
2021-11-24 16:17:54 +0100

    working on automating the whole thing

--- moxa_telnet.py
+++ moxa_telnet.py
@@ -3,9 +3,10 @@
 
 """
 TODO:
-    * class to keep connection
-    * keep connection alive: 2min timeout
-    * automate everything
+    [x] class to keep connection
+    [ ] keep connection alive: 2min timeout
+    [ ] automate everything
+    [ ] get macaddress(and other info) and note switch as done in file
 
 ACTION ORDER:
     1. Update Hostname
@@ -23,29 +24,44 @@ from multiprocessing import Process
 user = 'admin'
 password = ''
 sleep_time = 0.5
-fw_file = 'EDS408A_V3.8.rom'
+fw_file = ['EDS408A_V3.8.rom', 3.8]
 ethernet_card = "enp0s31f6"
 switch_pre_ip = "192.168.127.253"
-laptop_pre_ip ='192.168.127.200'
+client_default_ip ='192.168.127.200'
 
 
 def get_ip():
+    """
+    get client ip and return it as a string
+    """
     get_ip = subprocess.run(["ip addr show " + ethernet_card], shell=True, capture_output=True, encoding='ascii')
     slicepos = get_ip.stdout.find('inet ')
     current_ip = ''.join(get_ip.stdout[slicepos+5:slicepos+20])
     return current_ip
 
-def change_ip(ip, current_ip):
-    if ip != current_ip:
-        # remove from last dot, not last 3 spaces in case of ip address under 100:
-        set_ip = subprocess.run(["sudo ip addr add local " + ip + "/24 broadcast " + ip[:-3] + "255 dev " + ethernet_card], shell=True)
-        if set_ip.returncode == 0:
-            print("IP address set to: {}".format(ip))
-        else:
-            print("Could not set IP...")
-            exit(-1)
+def change_ip(ip):
+    """
+    change ip address of client to (ip)
+    """
+    # remove from last dot, not last 3 spaces in case of ip address under 100:
+    set_ip = subprocess.run(["sudo ip addr add local " + ip + "/24 broadcast " + ip[:-3] + "255 dev " + ethernet_card], shell=True)
+    if set_ip.returncode == 0:
+        print("IP address set to: {}".format(ip))
     else:
-        print("Already have IP: ", current_ip)
+        print("Could not set IP...")
+        exit(-1)
+
+def make_alarmdict(ifacelist, alarmlist, clientport):
+    alarmdict = {}
+    for i in range(len(ifacelist)):
+        if i != int(clientport) -1:
+            if ifacelist[i] == 'Up' and alarmlist[i] != 'Off':
+                alarmdict[i+1] = 1
+            elif ifacelist[i] == 'Down' and alarmlist[i] == 'Off':
+                alarmdict[i+1] = 0
+        else:
+            alarmdict[i+1] = 0
+    return alarmdict
 
 
 class Connection:
@@ -110,8 +126,8 @@ class Connection:
         """
         self.p.start()
         self.tn.write(b'copy tftp device-firmware\n')
-        self.tn.write(laptop_pre_ip.encode('utf-8') + b'\n')
-        self.tn.write(fw_file.encode('utf-8') + b'\n')
+        self.tn.write(client_default_ip.encode('utf-8') + b'\n')
+        self.tn.write(fw_file[0].encode('utf-8') + b'\n')
         print(self.tn.read_until(b'Download OK !!!', 5.0).decode('ascii'))
         print('Switch is rebooting.')
         self.p.terminate()
@@ -132,7 +148,7 @@ class Connection:
         """
         self.tn.write(b'show version\n')
         version = self.tn.read_until(b'EDS-408A-MM-SC#').decode('ascii')
-        return re.findall('(?<=: ).*', version)
+        return re.findall('(?<=: ).*', version.strip())
 
     def get_ifaces(self):
         """
@@ -154,7 +170,7 @@ class Connection:
         """
         self.p.start()
         self.tn.write(b'copy running-config tftp ')
-        self.tn.write(b'tftp://' + laptop_pre_ip.encode('ascii') + b'/running.ini\n')
+        self.tn.write(b'tftp://' + client_default_ip.encode('ascii') + b'/running.ini\n')
         print(self.tn.read_until(b'Upload Ok !!!', 5.0).decode('ascii'))
         self.p.terminate()
         self.p.join()
@@ -166,7 +182,7 @@ class Connection:
         """
         self.p.start()
         self.tn.write(b'copy startup-config tftp\n')
-        self.tn.write(laptop_pre_ip.encode('ascii') + b'\n')
+        self.tn.write(client_default_ip.encode('ascii') + b'\n')
         self.tn.write(b'sys.ini\n')
         print(self.tn.read_until(b'Upload Ok !!!', 5.0).decode('ascii'))
         self.p.terminate()
@@ -200,40 +216,45 @@ class Connection:
         """
         self.tn.write(b'login mode menu\n')
 
-    def conf_iface(self, iface, switch):
+    def conf_iface(self, alarmdict):
         """
-        Configures alarm for (iface). (switch) == 1 is alarm on
+        Configures alarm for interfaces in alarmdict. value == 1 is alarm on
         """
         self.tn.write(b'configure\n')
         self.tn.read_until(b'EDS-408A-MM-SC(config)#')
-        self.tn.write(b'interface ethernet 1/'+ iface.encode('ascii') + b'\n')
-        self.tn.read_until(b'EDS-408A-MM-SC(config-if)#')
-        if switch == 1:
-            self.tn.write(b'relay-warning event link-off\n')
+        for iface in alarmdict:
+            self.tn.write(b'interface ethernet 1/'+ str(iface).encode('ascii') + b'\n')
             self.tn.read_until(b'EDS-408A-MM-SC(config-if)#')
-            self.tn.write(b'exit\n')
-            self.tn.read_until(b'EDS-408A-MM-SC(config)#')
-            self.tn.write(b'exit\n')
-            self.tn.read_until(b'EDS-408A-MM-SC#')
-        else:
-            self.tn.write(b'no relay-warning event link\n')
-            self.tn.read_until(b'EDS-408A-MM-SC(config-if)#')
-            self.tn.write(b'exit\n')
-            self.tn.read_until(b'EDS-408A-MM-SC(config)#')
-            self.tn.write(b'exit\n')
-            self.tn.read_until(b'EDS-408A-MM-SC#')
+            if alarmdict[iface] == 1:
+                self.tn.write(b'relay-warning event link-off\n')
+                self.tn.read_until(b'EDS-408A-MM-SC(config-if)#')
+                self.tn.write(b'exit\n')
+                self.tn.read_until(b'EDS-408A-MM-SC(config)#')
+            else:
+                self.tn.write(b'no relay-warning event link\n')
+                self.tn.read_until(b'EDS-408A-MM-SC(config-if)#')
+                self.tn.write(b'exit\n')
+                self.tn.read_until(b'EDS-408A-MM-SC(config)#')
+        self.tn.write(b'exit\n')
+        self.tn.read_until(b'EDS-408A-MM-SC#')
 
     def conf_ip(self, ip):
+                """
+                Changes the ip-address of the switch to (ip)
+                """
                 self.tn.write(b'configure\n')
                 self.tn.read_until(b'EDS-408A-MM-SC(config)#')
                 self.tn.write(b'interface mgmt\n')
                 self.tn.read_until(b'EDS-408A-MM-SC(config-vlan)#')
-                ip = input('Please input last digits of ip: ').encode('ascii')
-                self.tn.write(b'ip address static 192.168.127.' + ip + b' 255.255.255.0\n')
+                self.tn.write(b'ip address static 192.168.127.' +
+                        ip.encode('ascii') + b' 255.255.255.0\n')
                 self.tn.write(b'exit\n')
                 self.tn.read_until(b'EDS-408A-MM-SC#')
 
     def conf_hostname(self, hostname):
+                """
+                Changes the hostname of the switch to (hostname)
+                """
                 self.tn.write(b'configure\n')
                 self.tn.read_until(b'EDS-408A-MM-SC(config)#')
                 self.tn.write(b'hostname ' + hostname.encode('ascii') + b'\n')
@@ -242,33 +263,41 @@ class Connection:
                 self.tn.read_until(b'EDS-408A-MM-SC#')
 
 
-
 if __name__ == "__main__":
-    moxa_switch = Connection(switch_pre_ip)
-    login_mode = moxa_switch.check_login()
-    if login_mode[0] == 0:
-        moxa_switch.menu_login()
-    elif login_mode[0] == 1:
-        moxa_switch.cli_login()
-    else:
-        print("Unknown login, exiting...")
-        exit(-1)
-
-    # while True:
-    #     hostname = input('Hostname: ')
-    #     switch_new_ip = input('Last 3 digits of switch ip: ')
-    #     current_ip = get_ip()
-    #     if current_ip == laptop_pre_ip:
-    #         login(switch_pre_ip)
-    #         break
-    #     else:
-    #         print('1. Telnet options')
-    #         print('2. Change laptop ip')
-    #         choice = input('>_  ')
-    #         if str(choice) == '':
-    #             break
-    #         if int(choice) == 1:
-    #             login(switch_pre_ip)
-    #             break
-    #         if int(choice) == 2:
-    #             change_ip(laptop_pre_ip, current_ip)
+    while True:
+        # -----------[ client ]---------------
+        current_ip = get_ip()
+        if current_ip != client_default_ip:
+            ipchange = input('Default IP doesnt match, change? (y/n)')
+            if ipchange.lower() == 'y':
+                change_ip(client_default_ip) 
+        # -----------[ switch ]---------------
+        hostname = input('Enter hostname for the switch: ')
+        clientport = input('Which port is the client attached to: ')
+        ip_add = input('Enter the last three digits of the new ip-address: ')
+        moxa_switch = Connection(switch_pre_ip)
+        login_mode = moxa_switch.check_login()
+        if login_mode[0] == 0:
+            moxa_switch.menu_login()
+            print('Reestablishing connection')
+            sleep(5)
+        elif login_mode[0] == -1:
+            print("Unknown login, exiting...")
+            break
+        elif login_mode[0] == 1:
+            moxa_switch.cli_login()
+            moxa_switch.conf_hostname(hostname)
+            moxa_switch.conf_iface(make_alarmdict(moxa_switch.get_ifaces(), moxa_switch.get_portconfig(), clientport))
+            if float(moxa_switch.get_version()[1][1:]) < fw_file[1]:
+                # moxa_switch.push_firmware()
+                print('pushing firmware')
+                print('Waiting 10s for reboot')
+                sleep(10)
+            if current_ip == client_default_ip:
+                # moxa_switch.conf_ip(ip_add)
+                print('changing ip address to 172.168.16.{}'.format(ip_add))
+                print('Waiting 5 seconds, then reconnect')
+                # change client ip address
+                # reconnect with new ip address
+                # get info and save to file
+            break

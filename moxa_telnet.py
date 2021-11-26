4ba27a4b448ac677b267d99b9f7e809823b425b2
Stian Knudsen <dislabled@gmail.com>
2021-11-26 16:21:06 +0100

    production ready

--- moxa_telnet.py
+++ moxa_telnet.py
@@ -25,11 +25,10 @@ user = 'admin'
 password = ''
 sleep_time = 0.5
 fw_file = ['EDS408A_V3.8.rom', 3.8]
-ethernet_card = "enp0s31f6"
-switch_pre_ip = "192.168.127.253"
-client_default_ip ='192.168.127.200'
-client_moxa_ip = '172.168.16.200'
-
+ethernet_card = 'enp0s31f6'
+client_ip ='192.168.127.200'
+client_ip2 = '172.168.16.200'
+switch_def_ip = '192.168.127.253'
 
 def get_ip():
     """
@@ -38,6 +37,7 @@ def get_ip():
     get_ip = subprocess.run(["ip addr show " + ethernet_card], shell=True, capture_output=True, encoding='ascii')
     slicepos = get_ip.stdout.find('inet ')
     current_ip = ''.join(get_ip.stdout[slicepos+5:slicepos+20])
+    print(current_ip)
     return current_ip
 
 def change_ip(ip, ip2):
@@ -45,8 +45,8 @@ def change_ip(ip, ip2):
     change ip address of client to (ip)
     """
     # remove from last dot, not last 3 spaces in case of ip address under 100:
-    set_ip = subprocess.run(["sudo ip addr add local " + ip2 + "/24 broadcast " + ip[:-3] + "255 dev " + ethernet_card], shell=True)
-    set_ip = subprocess.run(["sudo ip addr add local " + ip + "/24 broadcast " + ip[:-3] + "255 dev " + ethernet_card], shell=True)
+    set_ip = subprocess.run(['sudo ip addr add local ' + ip + '/24 broadcast ' + ip[:-3] + '255 dev ' + ethernet_card], shell=True)
+    set_ip = subprocess.run(['sudo ip addr add local ' + ip2 + '/24 broadcast ' + ip[:-3] + '255 dev ' + ethernet_card], shell=True)
     if set_ip.returncode == 0:
         print("IP address set to: {}".format(ip))
     else:
@@ -128,7 +128,7 @@ class Connection:
         """
         self.p.start()
         self.tn.write(b'copy tftp device-firmware\n')
-        self.tn.write(client_default_ip.encode('utf-8') + b'\n')
+        self.tn.write(client_ip.encode('utf-8') + b'\n')
         self.tn.write(fw_file[0].encode('utf-8') + b'\n')
         print(self.tn.read_until(b'Download OK !!!', 5.0).decode('ascii'))
         print('Switch is rebooting.')
@@ -142,7 +142,7 @@ class Connection:
         """
         self.tn.write(b'show system\n')
         sysinfo = self.tn.read_until(b'EDS-408A-MM-SC#').decode('ascii')
-        return re.findall('(?<=: ).*', sysinfo)
+        return re.findall('(?<=: )(.*)\\r', sysinfo)
 
     def get_version(self):
         """
@@ -150,7 +150,7 @@ class Connection:
         """
         self.tn.write(b'show version\n')
         version = self.tn.read_until(b'EDS-408A-MM-SC#').decode('ascii')
-        return re.findall('(?<=: ).*', version.strip())
+        return re.findall('(?<=: )(.*)\\r', version.strip())
 
     def get_ifaces(self):
         """
@@ -172,7 +172,7 @@ class Connection:
         """
         self.p.start()
         self.tn.write(b'copy running-config tftp ')
-        self.tn.write(b'tftp://' + client_default_ip.encode('ascii') + b'/running.ini\n')
+        self.tn.write(b'tftp://' + client_ip.encode('ascii') + b'/running.ini\n')
         print(self.tn.read_until(b'Upload Ok !!!', 5.0).decode('ascii'))
         self.p.terminate()
         self.p.join()
@@ -184,7 +184,7 @@ class Connection:
         """
         self.p.start()
         self.tn.write(b'copy startup-config tftp\n')
-        self.tn.write(client_default_ip.encode('ascii') + b'\n')
+        self.tn.write(client_ip.encode('ascii') + b'\n')
         self.tn.write(filename.encode('ascii') + b'_sys.ini\n')
         print(self.tn.read_until(b'Upload Ok !!!', 5.0).decode('ascii'))
         self.p.terminate()
@@ -195,22 +195,20 @@ class Connection:
         """
         temporary function to showcase the all the info
         """
-        print('Hostname: ' + Connection.get_sysinfo(self)[0])
-        print('FW Version: ' + Connection.get_version(self)[1])
         counter1 = 1
         portliste = []
         for port in Connection.get_portconfig(self):
             if port == 'Off':
                 portliste.append(counter1)
             counter1 += 1
-        print('Interfaces configured with alarm: {}'.format(portliste))
         ifaceliste = []
         counter2 = 1
         for port in Connection.get_ifaces(self):
             if port == 'Up':
                 ifaceliste.append(counter2)
             counter2 += 1
-        print('Interfaces in use: {}'.format(ifaceliste))
+        print('{:<35}{}'.format('Interfaces configured with alarm: ', portliste))
+        print('{:<35}{}'.format('Interfaces in use: ', ifaceliste))
 
     def login_change(self):
         """
@@ -270,19 +268,31 @@ class Connection:
         self.tn.read_until(b'EDS-408A-MM-SC#')
         return status
 
+def parse_list(list, keyword):
+    """
+    Parses the port/or alarmlist(list). (keyword) triggers on.
+    """
+    cnt = 1
+    ports = []
+    for port in list:
+        if port == keyword:
+            ports.append(cnt)
+        cnt += 1
+    return ports
+
 if __name__ == "__main__":
     while True:
         # -----------[ client ]---------------
         current_ip = get_ip()
-        if current_ip != client_default_ip:
+        if current_ip != client_ip:
             ipchange = input('Default IP doesnt match, change? (y/n)')
             if ipchange.lower() == 'y':
-                change_ip(client_default_ip, client_moxa_ip) 
+                change_ip(client_ip, client_ip2) 
         # -----------[ switch ]---------------
         hostname = input('Enter hostname for the switch: ')
         clientport = input('Which port is the client attached to: ')
         ip_add = input('Enter the last three digits of the new ip-address: ')
-        moxa_switch = Connection(switch_pre_ip)
+        moxa_switch = Connection(switch_def_ip)
         login_mode = moxa_switch.check_login()
         if login_mode[0] == 0:
             moxa_switch.menu_login()
@@ -293,6 +303,19 @@ if __name__ == "__main__":
             break
         elif login_mode[0] == 1:
             moxa_switch.cli_login()
+            version = moxa_switch.get_version()
+            system = moxa_switch.get_sysinfo()
+            portlist = parse_list(moxa_switch.get_portconfig(), 'Off')
+            ifacelist = parse_list(moxa_switch.get_ifaces(), 'Up')
+            print('-'*80)
+            print('{:<45}{}\n{:<45}{}\n{}'.format('Switch Name: ' + system[0], 'Switch Location: ' + system[1],
+                'Switch Description: ' + system[2], 'MAC Address: ' + system[4], 'Uptime: ' + system[5]))
+            print('{:<45}{}'.format('Model Version: ' + version[0], 'FW Version: ' +  version[1]))
+            print('{:<35}{}'.format('Interfaces configured with alarm: ', portlist))
+            print('{:<35}{}'.format('Interfaces in use: ', ifacelist))
+            print('-'*80)
+            if input('continue? (y/n)').lower() != 'y':
+                break
             print('Changing Hostname: {}'.format(hostname))
             moxa_switch.conf_hostname(hostname)
             print('Checking and setting port alarms...')
@@ -304,7 +327,7 @@ if __name__ == "__main__":
                 print('Waiting 10s for reboot')
                 sleep(10)
             print('Checking and changing IP address of switch: {}'.format('172.168.16.'+ ip_add))
-            if current_ip == client_default_ip:
+            if current_ip == client_ip:
                 moxa_switch.conf_ip(ip_add)
                 print('changing ip address to 172.168.16.{}'.format(ip_add))
                 print('Waiting 5 seconds, then reconnect')
@@ -314,9 +337,18 @@ if __name__ == "__main__":
                     moxa_switch.cli_login()
                     if moxa_switch.save()[0] == 0:
                         system = moxa_switch.get_sysinfo()
+                        version = moxa_switch.get_version()
+                        portlist = parse_list(moxa_switch.get_portconfig(), 'Off')
+                        ifacelist = parse_list(moxa_switch.get_ifaces(), 'Up')
                         moxa_switch.get_startup_conf('configs/' + system[0])
                         logfile.write('\n' + system[4] + ' - ' + system[0] + ' - 172.168.16.' + ip_add + ' - DONE')
-                        print('Everything done!')
+                        print('-'*80)
+                        print('{:<45}{}\n{:<45}{}\n{}'.format('Switch Name: ' + system[0], 'Switch Location: ' + system[1],
+                            'Switch Description: ' + system[2], 'MAC Address: ' + system[4], 'Uptime: ' + system[5]))
+                        print('{:<45}{}'.format('Model Version: ' + version[0], 'FW Version: ' +  version[1]))
+                        print('{:<35}{}'.format('Interfaces configured with alarm: ', portlist))
+                        print('{:<35}{}'.format('Interfaces in use: ', ifacelist))
+                        print('-'*80)
                     else:
                         print('Saving to eeprom failed')
             break
